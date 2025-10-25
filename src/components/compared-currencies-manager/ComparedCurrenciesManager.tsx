ad61050878f1df077d81a2bfbc6464cb83f1a1a4
yaroslav_perets <Yaroslav.Perets@dataart.com>
2025-10-25 15:15:38 +0300

    Fix delete currency logic

--- src/components/compared-currencies-manager/ComparedCurrenciesManager.tsx
+++ src/components/compared-currencies-manager/ComparedCurrenciesManager.tsx
@@ -1,5 +1,5 @@
 import { Stack, Typography, Autocomplete, TextField } from '@mui/material';
-import { addCompared } from '../../features/currencies/currenciesSlice';
+import { addCompared, removeCompared } from '../../features/currencies/currenciesSlice';
 import { useAppDispatch } from '../../app/hooks.ts';
 import { MAX_COMPARED_CURRENCIES, MIN_COMPARED_CURRENCIES } from '../../features/currencies/constants';
 import { useComparedCurrencies } from './useComparedCurrencies';
@@ -9,9 +9,18 @@ export default function ComparedCurrenciesManager() {
 	const dispatch = useAppDispatch();
 	const { compared, available } = useComparedCurrencies();
 
+	const handleAddCurrency = (_: unknown, value: string | null) => {
+		if (value) {
+			dispatch(addCompared(value.toLowerCase()));
+		}
+	};
+
+	const handleDelete = (code: string) => {
+		dispatch(removeCompared(code));
+	};
+
 	return (
 		<Stack spacing={1}>
-			{/* ---------- Add Currency ---------- */}
 			<Typography
 				variant="subtitle2"
 				sx={{ mb: 0.5, color: 'text.secondary', fontWeight: 500 }}
@@ -26,14 +35,17 @@ export default function ComparedCurrenciesManager() {
 				renderInput={(params) => (
 					<TextField {...params} placeholder="Search..." />
 				)}
-				onChange={(_, value) => {
-					if (value) dispatch(addCompared(value.toLowerCase()));
-				}}
+				onChange={handleAddCurrency}
 				blurOnSelect
 				clearOnEscape
 			/>
 
-			<ComparedCurrenciesList compared={compared} min={MIN_COMPARED_CURRENCIES} max={MAX_COMPARED_CURRENCIES} />
+			<ComparedCurrenciesList
+				compared={compared}
+				min={MIN_COMPARED_CURRENCIES}
+				max={MAX_COMPARED_CURRENCIES}
+				onDelete={handleDelete}
+			/>
 		</Stack>
 	);
 }
